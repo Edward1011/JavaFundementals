b3f54dd00e1096f539785fdc2d35c50e4e9f0411
Edward Marin <FlameholdLagoon@Gmail.com>
2019-06-13 16:02:45 -0500

    Lesson 4 - My Application Overview

---
+++ src/com/fundamental/java/House.java
@@ -0,0 +1,15 @@
+package com.fundamental.java;
+/* first class
+we created
+ourselves
+*/
+public class House {
+    String roofType;
+    int windowType;
+    String doorColor;
+    String foundation;
+//Our first method(Fuction)
+    public void doorOpenClose(){
+    System.out.println("Open my door");
+    }
+}
--- src/com/fundamental/java/Main.java
+++ src/com/fundamental/java/Main.java
@@ -1,8 +1,15 @@
 package com.fundamental.java;
 
 public class Main {
+/*
+this class is the starting class of this project
+this is on example of split -line comment-
 
+ */
     public static void main(String[] args) {
+
         System.out.println("Welcome to Java");
+        House myHouse= new House();
+        myHouse.doorOpenClose();
     }
 }
